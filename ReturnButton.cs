9ffeffacf0edac10337be2c7cf42d937e70a44f5
YoikaMinato <r.sun0302@gmail.com>
2020-08-12 22:52:44 +0900

    3

---
+++ ReturnButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class ReturnButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ボタンをクリックするとワールドツアー画面へ戻る関数
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
