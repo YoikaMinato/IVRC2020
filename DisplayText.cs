6d63a825b44af318dd919e2abba67379b40e5830
YoikaMinato <r.sun0302@gmail.com>
2020-08-12 22:52:13 +0900

    2

---
+++ DisplayText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayText : MonoBehaviour
+{
+    public GameObject ansText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameManager.getDisplayAns())
+        {
+            ansText.gameObject.SetActive(true);
+        }
+        else
+        {
+            ansText.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
